b8d7eecc046666a5bb39e8a99fdf8fcaa35a89a3
notlemos <caiolemossfc123@hotmail.com>
2026-03-08 17:33:06 -0300

    Criando componentes - Criando componente InputCheckbox

---
+++ src/App.tsx
@@ -0,0 +1,63 @@
+import Text from "./components/text";
+import TrashIcon from "./assets/icons/trash.svg?react"
+import CheckIcon from "./assets/icons/check.svg?react"
+import PencilIcon from "./assets/icons/pencil.svg?react"
+import PlusIcon from "./assets/icons/plus.svg?react"
+import SpinnerIcon from "./assets/icons/spinner.svg?react"
+import XIcon from "./assets/icons/x.svg?react"
+import Icon from "./components/icon";
+import Badge from "./components/badge";
+import Button from "./components/button";
+import ButtonIcon from "./components/button-icon";
+import InputText from "./components/input-text";
+import InputCheckbox from "./components/input-check-box";
+
+
+export default function App() {
+  return (
+    <div className="grid gap-10">
+      <div className="flex flex-col gap-2">
+        <Text variant="body-sm-bold" className="text-pink-base">
+          Olá Mundo!
+        </Text>
+        <Text className="text-green-base">Olá Mundo!</Text>
+        <Text  variant="body-md-bold" >Olá Mundo!</Text>
+        <Text>Levar dog para passear!</Text>
+      </div>
+      <div className="flex gap-1">
+        <Icon svg={TrashIcon} className="fill-green-base"/>
+        <Icon svg={CheckIcon}/>
+        <Icon svg={PlusIcon}/>
+        <Icon svg={PencilIcon}/>
+        <Icon svg={SpinnerIcon} animate/>
+        <Icon svg={XIcon}/>
+      </div>
+
+      <div>
+        <Badge variant={"secondary"}>5</Badge>
+        <Badge variant={"primary"}>2 de 5</Badge>
+      </div>
+
+      <div>
+        <Button icon={PlusIcon}>Nova Tarefa</Button>
+      </div>
+
+      <div className="flex-gap-1">
+        <ButtonIcon icon={TrashIcon}/>
+        <ButtonIcon icon={TrashIcon} variant="secondary"/>
+        <ButtonIcon icon={TrashIcon} variant="tertiary"/>
+      </div>
+
+      <div>
+        <InputText/>
+      </div>
+
+      <div>
+        <InputCheckbox></InputCheckbox>
+      </div>
+
+    </div>
+    
+  )
+  
+}
