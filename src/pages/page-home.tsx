d0af19f067c23ac35afa154362fc5994af065329
notlemos <caiolemossfc123@hotmail.com>
2026-03-09 20:18:55 -0300

    Montando Layout: Criando Componente da Tarefa

--- src/pages/page-home.tsx
+++ src/pages/page-home.tsx
@@ -1,4 +1,5 @@
 import Container from "../components/container";
+import TaskItem from "../core-components/task-item";
 import TasksSummary from "../core-components/tasks-summary";
 
 export default function PageHome() {
@@ -6,5 +7,7 @@ export default function PageHome() {
         <header className="flex items-center justify-between">
             <TasksSummary />
         </header>
+
+        <TaskItem/>
     </Container>
 }
